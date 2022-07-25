2d6b8b03e69566326a358c3a5dc73a2c03fe6fde
cuong-pm <cuong-pm@blockhive.vn>
2022-07-25 10:47:31 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-main>
+        <Nuxt />
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
